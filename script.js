56c6b65fe0829aa23b267c2030bc02ebfcca3f21
Wilson <109673935+wilsongneto@users.noreply.github.com>
2026-04-30 09:17:17 -0300

    js

--- script.js
+++ script.js
@@ -26,3 +26,27 @@ function salvarCarro(event) {
 
     document.querySelector("form").reset();
 }
+
+function adicionarNaTela(carro) {
+    let lista = document.getElementById('listarCarros');
+    let card = document.createElement('div');
+    card.classList.add('card');
+
+    card.innerHTML = `
+    <h3>${carro.titulo}</h3>
+    <p><strong>Preço:</strong> R$ ${carro.preco}</p>
+    <p><strong>Marca:</strong> R$ ${carro.marca}</p>
+    <p><strong>Modelo:</strong> R$ ${carro.modelo}</p>
+    <p><strong>Câmbio:</strong> R$ ${carro.cambio}</p>
+    `
+
+    lista.appendChild(card);
+}
+
+window.onload = function () {
+    let carros = JSON.parse(localStorage.getItem('carros')) || [];
+
+    carros.forEach(carro => {
+        adicionarNaTela(carro);
+    });
+}
